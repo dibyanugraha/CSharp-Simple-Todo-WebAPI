810207b9477f3d7861904795aaae73b6437960aa
Ariestya Dibyanugraha <ariestya.dibyanugraha@gmail.com>
2019-04-23 13:08:45 +0700

    Implement HTTP Delete Todo

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TodoApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace TodoApi.Controllers
 {
@@ -73,6 +74,40 @@ namespace TodoApi.Controllers
 
             await _context.SaveChangesAsync();
             
+            return NoContent();
+        }
+        #endregion
+
+        #region HTTP Patch Todo
+        // Todo: Finish patch implementation
+        [HttpPatch("{id}")]
+
+        public async Task<ActionResult<Todo>> PatchTodo(int id, [FromBody]JsonPatchDocument<Todo> todoPatch)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            
+            todoPatch.ApplyTo(todo);
+
+            _context.Update(todo);
+
+            return todo;
+        }
+        #endregion
+
+        #region HTTP DELETE Todo
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+
             return NoContent();
         }
         #endregion
